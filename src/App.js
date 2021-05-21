319a28c2138eba45448be1d896a6802ac459b0f0
Debanjan Acharyya <70289645+debanjan-exe@users.noreply.github.com>
2021-05-21 14:32:48 +0530

    update hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import Home from "./components/Home";
 import CourseStructure from "./components/CourseStructure";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
        <Route exact path="/" component={Home} />
        <Route path="/:coursename" component={CourseStructure} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
